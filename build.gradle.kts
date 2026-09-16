916579fb7344d7a07ac0f671ac6c9cba76103ce2
adenhernandez30-hub <adenhernandez30@gmail.com>
2026-09-16 17:18:38 +0700

    chore: migrate provider build configuration

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+group = "com.kakaanime"
+version = "0.1.0"
+
+plugins {
+    id("com.android.library") version "8.9.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+}
