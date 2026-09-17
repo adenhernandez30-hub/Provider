372d512e4f95476080bf811b731e3908dbaeb077
adenhernandez30-hub <adenhernandez30@gmail.com>
2026-09-17 14:14:41 +0700

    test(wibuku): enable JVM unit tests

--- provider/build.gradle.kts
+++ provider/build.gradle.kts
@@ -29,4 +29,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jsoup:jsoup:1.18.3")
+    testImplementation("junit:junit:4.13.2")
 }
