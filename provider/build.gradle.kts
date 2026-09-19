92d524abc0a5a099dc6d63090adb035c21285319
adenhernandez30-hub <adenhernandez30@gmail.com>
2026-09-19 19:50:38 +0700

    test(provider): add real-site E2E test dependency

--- provider/build.gradle.kts
+++ provider/build.gradle.kts
@@ -25,6 +25,7 @@ android {
 }
 
 dependencies {
+    testImplementation("junit:junit:4.13.2")
     implementation(project(":provider-core"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
