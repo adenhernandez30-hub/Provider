5eeca184cc0f58f8f09f8e44f912c27dde81c95d
adenhernandez30-hub <adenhernandez30@gmail.com>
2026-09-19 19:50:51 +0700

    fix(provider-e2e): use JUnit assertions

--- provider/src/test/java/com/kakaanime/provider/ProviderRealSiteE2ETest.kt
+++ provider/src/test/java/com/kakaanime/provider/ProviderRealSiteE2ETest.kt
@@ -3,7 +3,7 @@ package com.kakaanime.provider
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeoutOrNull
 import org.junit.Test
-import kotlin.test.assertTrue
+import org.junit.Assert.assertTrue
 
 class ProviderRealSiteE2ETest {
 
