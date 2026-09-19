49008e88abbe4072d3b2d6a66c6d7b281954058d
adenhernandez30-hub <adenhernandez30@gmail.com>
2026-09-19 22:02:21 +0700

    test: add provider v2 verifier test dependencies

--- provider-v2/build.gradle.kts
+++ provider-v2/build.gradle.kts
@@ -24,4 +24,8 @@ android {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
 }
